4ca80ba741bc68519a9b311f3a3ce8cf66194fdf
Andre Ohuti <ohuti@hotmail.com>
2016-04-01 16:06:29 -0300

    calculadora

--- calculadora.c
+++ calculadora.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-int A, B, C, TOTAL1, TOTAL2;
-printf("Adcione o primeiro termo: \n", A);
-scanf("%d", &A);
-printf("Adcione o segundo termo: \n", B);
-scanf("%d", &B);
-printf("Adcione o terceiro termo: \n", C);
-scanf("%d", &C);
+double A, B, C, TOTAL1, TOTAL2;
+scanf("%lf", &A);
+scanf("%lf", &B);
+scanf("%lf", &C);
 TOTAL1 = (-B + (B^2 - 4*A*C)^0.5) / 2*A;
 TOTAL2 = (-B - (B^2 - 4*A*C)^0.5) / 2*A;
-printf("Resultado 1 = %d\n", TOTAL1);
-printf("            = %d\n", TOTAL2);
+printf("Resultado 1 = %lf\n", TOTAL1);
+printf("            = %lf\n", TOTAL2);
 return 0;
 }

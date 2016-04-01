8810d942ae280fa145d2ca3c39c998948f2052b5
Andre Ohuti <ohuti@hotmail.com>
2016-04-01 16:27:53 -0300

    calculadora

--- calculadora.c
+++ calculadora.c
@@ -2,16 +2,21 @@
 #include <math.h>
 
 int main(void) {
-double A, B, C, TOTAL1, TOTAL2;
+double A, B, C, DELTA, TOTAL1, TOTAL2;
 printf("Digite primeiro termo: ");
 scanf("%lf", &A);
-printf("\n Digite segundo termo: ");
+printf("Digite segundo termo: ");
 scanf("%lf", &B);
-printf("\n Digite terceiro termo: ");
+printf("Digite terceiro termo: ");
 scanf("%lf", &C);
-TOTAL1 = (-B + sqrt(pow(B, 2) - 4*A*C)) / 2*A;
-TOTAL2 = (-B - sqrt(pow(B, 2) - 4*A*C))/ 2*A;
+DELTA = pow(B ,2) - 4*A*C;
+TOTAL1 = (-B + sqrt(DELTA)) / 2*A;
+TOTAL2 = (-B - sqrt(DELTA)) / 2*A;
+if(DELTA > 0) {
 printf("Resultado 1 = %lf\n", TOTAL1);
 printf("            = %lf\n", TOTAL2);
+} else {
+	printf("Erro, Delta menor que 0\n");
 return 0;
+}
 }

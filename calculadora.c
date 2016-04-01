7d3b08c020c4151354f7dcda8853b538860c9e06
Andre Ohuti <ohuti@hotmail.com>
2016-04-01 16:20:05 -0300

    calculadora

--- calculadora.c
+++ calculadora.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
 double A, B, C, TOTAL1, TOTAL2;
 printf("Digite primeiro termo: ");
 scanf("%lf", &A);
-printf("Digite segundo termo: ");
+printf("\n Digite segundo termo: ");
 scanf("%lf", &B);
-printf("Digite terceiro termo: ");
+printf("\n Digite terceiro termo: ");
 scanf("%lf", &C);
-TOTAL1 = (-B + (B*B - 4*A*C)^0.5) / 2*A;
-TOTAL2 = (-B - (B*B - 4*A*C)^0.5) / 2*A;
+TOTAL1 = (-B + sqrt(pow(B, 2) - 4*A*C)) / 2*A;
+TOTAL2 = (-B - sqrt(pow(B, 2) - 4*A*C))/ 2*A;
 printf("Resultado 1 = %lf\n", TOTAL1);
 printf("            = %lf\n", TOTAL2);
 return 0;

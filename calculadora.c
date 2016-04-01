024174d06bf299a06ef4a185ad0b423cbe43a4e4
Andre Ohuti <ohuti@hotmail.com>
2016-04-01 16:14:11 -0300

    calculadora

--- calculadora.c
+++ calculadora.c
@@ -2,11 +2,14 @@
 
 int main(void) {
 double A, B, C, TOTAL1, TOTAL2;
+printf("Digite primeiro termo: ");
 scanf("%lf", &A);
+printf("Digite segundo termo: ");
 scanf("%lf", &B);
+printf("Digite terceiro termo: ");
 scanf("%lf", &C);
-TOTAL1 = (-B + (B^2 - 4*A*C)^0.5) / 2*A;
-TOTAL2 = (-B - (B^2 - 4*A*C)^0.5) / 2*A;
+TOTAL1 = (-B + (B*B - 4*A*C)^0.5) / 2*A;
+TOTAL2 = (-B - (B*B - 4*A*C)^0.5) / 2*A;
 printf("Resultado 1 = %lf\n", TOTAL1);
 printf("            = %lf\n", TOTAL2);
 return 0;
